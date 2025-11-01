5043734f80aab59ee82f2349ba3e2f58ee1ee403
Prerana-Naik <138115844+Prerana-Naik@users.noreply.github.com>
2025-11-01 18:26:26 +0530

    Simplify Next.js PWA configuration

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
 })
 
-const nextConfig: NextConfig = {
-  /* config options here */
-}
-
-module.exports = withPWA(nextConfig)
+module.exports = withPWA({})
